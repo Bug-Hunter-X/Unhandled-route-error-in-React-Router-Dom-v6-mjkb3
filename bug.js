8a099a9f74cc73fa32650d2ee3e5027faef86489
BugHunter-X <askzenai@gmail.com>
2025-02-18 04:30:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,27 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* Missing Route for path='/users/:id' */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <div>Home</div>;
+}
+
+function About() {
+  return <div>About</div>;
+}
+
+function User({ params }) {
+  return <div>User ID: {params.id}</div>;
+}
+```
